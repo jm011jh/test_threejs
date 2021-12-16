dec65e4a0c7f4e77c1147b829c69352792a04438
jm011jh <jm011jh@gmail.com>
2021-12-16 17:18:37 +0900

    sjm main.js

--- common/js/main.js
+++ common/js/main.js
@@ -6,6 +6,7 @@ camera.position.z = 25;
 camera.lookAt(scene.position)
 
 const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setClearColor(new THREE.Color(0xeeeeee));
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMapEnabled =true;
@@ -21,9 +22,9 @@ plane.position.z = 0;
 scene.add(plane)
 
 const axes = new THREE.AxisHelper(20);
-scene.add(axes);
+// scene.add(axes);
 
-const cubeGeometry = new THREE.BoxGeometry(4,4,4);
+const cubeGeometry = new THREE.BoxGeometry(5,5,5);
 const cubeMaterial = new THREE.MeshLambertMaterial({color:0x12db00, wireframe: false})
 const cube = new THREE.Mesh(cubeGeometry,cubeMaterial)
 cube.castShadow = true;
@@ -32,12 +33,52 @@ cube.position.y = 5;
 cube.position.z = 0;
 scene.add(cube)
 
-const spotLight = new THREE.SpotLight(0xffffff);
+function rotateCube(time){
+    time *= 0.0005;
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+    cube.rotation.z = time;
+
+    renderer.render(scene,camera);
+    requestAnimationFrame(rotateCube)
+}
+requestAnimationFrame(rotateCube)
+
+const spotLight = new THREE.SpotLight(0xffff00);
 spotLight.position.set(-40,60,-10);
 spotLight.castShadow = true;
 spotLight.shadowMapHeight = 5000;
 spotLight.shadowMapWidth = 5000;
 scene.add(spotLight)
 
+const spotLight2 = new THREE.SpotLight(0xcccccc);
+spotLight2.position.set(40,60,100);
+spotLight2.castShadow = true;
+spotLight2.shadowMapHeight = 5000;
+spotLight2.shadowMapWidth = 5000;
+scene.add(spotLight2)
+
+const line1points = [];
+line1points.push(new THREE.Vector3(-10,0,0))
+line1points.push(new THREE.Vector3(0,10,0))
+line1points.push(new THREE.Vector3(10,0,0))
+const line1Geometry = new THREE.BufferGeometry().setFromPoints(line1points);
+const line1Material = new THREE.LineBasicMaterial({color: 0x0000ff});
+const line1 = new THREE.Line(line1Geometry,line1Material)
+scene.add(line1)
+
+
 document.getElementById("WebGL-output").appendChild(renderer.domElement);
 renderer.render(scene, camera)
+
+window.addEventListener("resize",resize,false)
+function resize(){
+    renderer.setSize(window.innerWidth,window.innerHeight)
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        5
+    )
+    camera.position.z = 2;
+}
